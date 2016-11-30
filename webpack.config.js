2de334e73e1953bf2a4c39ced456832050824b39
Davide Cesaroni <davide.cesaroni@gmail.com>
2016-11-30 08:26:30 +0100

    Add project configurations

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    entry: ['babel-polyfill', './index.js'],
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            }
+        ],
+
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
