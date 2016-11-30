9107672f6c657a0f68c73a0fb5534a49b99e3aee
Davide Cesaroni <davide.cesaroni@gmail.com>
2016-11-30 21:11:41 +0100

    Fix wrong index.js

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>TEST</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head>
-  <body>
-    <div id="app"></div>
-    <script src="bundle.js"></script>
-  </body>
-</html>
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+import Board from './app/components/Board';
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.state.time = props.time;
+        this.state.request = 0;
+        this.state.prevTimestamp = 0;
+        this._requestAnimFrame = null;
+    }
+
+    tick(timestamp) {
+        if ((timestamp - this.state.prevTimestamp) > 1000 ) {
+            this.setState({
+                time: this.state.time + 1,
+                prevTimestamp: timestamp
+            });
+        }
+
+        this._requestAnimFrame = requestAnimationFrame(this.tick.bind(this));        
+    }
+
+    stop() {
+        if (this._requestAnimFrame) {
+            cancelAnimationFrame(this._requestAnimFrame);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.tick.bind(this)}>START</button>
+                <button onClick={this.stop.bind(this)}>STOP</button>
+                <Board text={this.state.time} time={this.state.time}/>
+            </div>
+            );
+    }
+}
+
+App.propTypes = {
+        time: PropTypes.number
+    }
+
+const element = <App time={0} text="0"/>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('app')
+);
+
+
+// function animate() {
+//     element.tick();
+//     requestAnimationFrame(animate);
+// }
+
+
+// animate();
