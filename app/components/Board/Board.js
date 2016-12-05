ce1d2eb801271f64ed9df297e434407121ce6a9c
Davide Cesaroni <davide.cesaroni@gmail.com>
2016-12-05 08:12:56 +0100

    Manage piece movement using the Piece class

--- app/components/Board/Board.js
+++ app/components/Board/Board.js
@@ -4,6 +4,7 @@ import update from 'immutability-helper';
 import styles from './styles';
 
 import Tile from '../Tile';
+import Piece from '../Piece';
 
 class Board extends Component {
 
@@ -23,11 +24,13 @@ class Board extends Component {
         this.state = {};
         this.state.board = new Array(Board.PROPERTIES.HEIGHT);
         this.state.board.fill(0);
-        this.state.board.map(() => {
+        this.state.board = this.state.board.map(() => {
             const inner = new Array(Board.PROPERTIES.WIDTH);
-            inner.fill(0);
+            inner.fill({val: 0, temporary: true});
             return inner;
         });
+
+        this.currentPiece = new Piece();
 
         styles.container = update(styles.container, {
             width: {$set: Board.PROPERTIES.WIDTH * Board.PROPERTIES.TILE_WIDTH},
@@ -36,16 +39,51 @@ class Board extends Component {
 
     }
 
+    _clearTemporaryTiles() {
+        this.state.board = this.state.board.map((row) => {
+            return row.map((singleTile) => {
+                if (singleTile.temporary === true) {
+                    return {val: 0, temporary: true};
+                }
+            });
+        });
+    }
+
     render() {
 
-        if (this.props.pressedKey === 'L') {
-            this._x = (this._x === 0) ? 0 : this._x - 1;
-        } else if (this.props.pressedKey === 'R') {
-            this._x = (this._x === Board.PROPERTIES.WIDTH - 1) ? Board.PROPERTIES.WIDTH - 1 : this._x + 1;
-        }
-        this._y = this.props.time;
+        console.log(this.currentPiece.shape.length);
 
-        return (<div style={styles.container}><Tile color="grey" x={this._x} y={this._y}/></div>);
+        this._clearTemporaryTiles();
+        if (this.props.pressedKey === 'E') {
+            this.currentPiece.rotate();
+        } else if (this.props.pressedKey === 'L') {
+            this.currentPiece.x = (this.currentPiece.x === 0) ? 0 : this.currentPiece.x - 1;
+        } else if (this.props.pressedKey === 'R') {
+            this.currentPiece.x = (this.currentPiece.x === Board.PROPERTIES.WIDTH - 1) ? Board.PROPERTIES.WIDTH - 1 : this.currentPiece.x + 1;
+        }
+
+        for (let i = 0; i < this.currentPiece.shape.length; i++) {
+            for (let j = 0; j < this.currentPiece.shape[i].length; j++) {
+                this.state.board[i + this.currentPiece.y][j + this.currentPiece.x] = {val: this.currentPiece.shape[i][j], temporary: true};
+            }
+        }
+
+        const tiles = this.state.board.reduce((accumulator, currentRow, currentRowIndex) => {
+            return accumulator.concat(
+                currentRow.map((singleTile, currentColIndex) => {
+                    if (singleTile.val !== 0) {
+                        return (<Tile key={currentRowIndex + '-' + currentColIndex} color="yellow" x={currentColIndex} y={currentRowIndex}/>);
+                    } else {
+                        return '';
+                    }
+                })
+            );
+        }, []);
+
+        
+        this.currentPiece.y = this.props.time;
+
+        return (<div style={styles.container}>{tiles}</div>);
     }
 }
 
